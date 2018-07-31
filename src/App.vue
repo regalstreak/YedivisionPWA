9f03e6b8a205377848abf8212ba9c8f3fab555a2
Neil Agarwal <regalstreak@gmail.com>
2018-08-01 02:13:58 +0530

    src: App.vue | Basic changes, give it a sidebar kinda thingy

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,36 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Yedivision</span>
+      </div>
     </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">Yedivision</span>
+      <nav class="mdl-navigation">
+        <router-link class="mdl-navigation__link" to="/" @click.native="hideMenu">Home</router-link>
+        <router-link class="mdl-navigation__link" to="/assignments" @click.native="hideMenu">Assignments</router-link>
+        <router-link class="mdl-navigation__link" to="/papers" @click.native="hideMenu">Question banks and papers</router-link>
+        <router-link class="mdl-navigation__link" to="/material" @click.native="hideMenu">Study material</router-link>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <router-view></router-view>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
+require("material-design-lite");
 export default {
   name: "app"
 };
 </script>
 
+
 <style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495e;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: 0.02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+@import url("https://code.getmdl.io/1.3.0/material.blue-red.min.css");
 </style>
